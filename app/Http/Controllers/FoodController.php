fe6deaab512159f2836e0b9b26f35e3a27c23273
abiram21 <ram.abiram21@gmail.com>
2019-09-20 12:14:38 +0530

    Add files via upload

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Food;
+use App\Client;
 use Illuminate\Http\Request;
+use App\Http\Requests\FoodStoreRequest;
+use App\Http\Requests\FoodUpdateRequest;
 
 class FoodController extends Controller
 {
@@ -14,7 +17,28 @@ class FoodController extends Controller
      */
     public function index()
     {
-        //
+     
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $food = Food::all();
+            $response['status'] = "Success";
+            $response['msg'] = "Get all Food";
+            $response['is_success'] = true;
+            $response['data'] = array('client'=>client::all(),'food'=>$food);
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -24,8 +48,11 @@ class FoodController extends Controller
      */
     public function create()
     {
-        //
+        $clients = Client::get()->pluck('phoneNo', 'id')->toArray();
+        return view('foods.create',compact('clients'));
+        
     }
+    
 
     /**
      * Store a newly created resource in storage.
@@ -33,9 +60,40 @@ class FoodController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(FoodStoreRequest $request)
     {
-        //
+    
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $food = new Food();
+
+                $food->name = $request->name;
+                $food->type = $request->type;
+                $food->minQty = $request->minQty;
+                $food->maxQty = $request->maxQty;
+                $food->unit_price = $request->unit_price;
+                $food->client_id = $request->client_id;
+                $food->fac_id = 1;
+
+                $food->save();
+            $response['status'] = "Success";
+            $response['msg'] = "Add Food";
+            $response['is_success'] = true;
+            $response['data'] =$food;
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -44,9 +102,29 @@ class FoodController extends Controller
      * @param  \App\Food  $food
      * @return \Illuminate\Http\Response
      */
-    public function show(Food $food)
+    public function show($id)
     {
-        //
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $food = Food::find($id);
+            $response['status'] = "Success";
+            $response['msg'] = "Get one Food";
+            $response['is_success'] = true;
+            $response['data'] = array('food'=>$food);
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -57,7 +135,8 @@ class FoodController extends Controller
      */
     public function edit(Food $food)
     {
-        //
+        $clients = Client::get()->pluck('phoneNo', 'id')->toArray();
+        return view('foods.edit', compact( 'clients'));
     }
 
     /**
@@ -67,9 +146,39 @@ class FoodController extends Controller
      * @param  \App\Food  $food
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Food $food)
+    public function update(Request $request,$id)
     {
-        //
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $food = Food::find($id);
+
+                $food->name = $request->name;
+                $food->type = $request->type;
+                $food->minQty = $request->minQty;
+                $food->maxQty = $request->maxQty;
+                $food->unit_price = $request->unit_price;
+                $food->client_id = $request->client_id;
+                $food->fac_id = 1;
+
+                $food->save();
+            $response['status'] = "Success";
+            $response['msg'] = "Update Food";
+            $response['is_success'] = true;
+            $response['data'] =$food;
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 
     /**
@@ -78,8 +187,35 @@ class FoodController extends Controller
      * @param  \App\Food  $food
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Food $food)
+
+    public function delete(Food $food)
     {
-        //
+        $clients = Client::get()->pluck('phoneNo', 'id')->toArray();
+        return view('foods.delete', compact('clients'));
+    }
+
+    public function destroy($id)
+    {
+        try{
+            $response = array(
+                'status'=>"Failed",
+                'msg'=>'',
+                'is_success'=>false,
+                'data'=>''
+            );
+            $food = Food::find($id);
+            $food->delete();
+            $response['status'] = "Success";
+            $response['msg'] = "Delect Food";
+            $response['is_success'] = true;
+            return response()->json($response);
+        }
+        catch(Exception $e)
+        {
+            $response['status'] = "Failed";
+            $response['msg'] = $e->getMessage();
+            $response['is_success'] = false;
+            return response()->json($response); 
+        }
     }
 }
